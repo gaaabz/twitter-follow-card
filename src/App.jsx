9f3ccd470a082c2b49fc4d9ce46d66e78d86466c
Gabriel Sosa <gabriel@codea.pe>
2023-04-07 20:59:33 -0500

    Feature: Add users array

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,25 @@ import Card from './components/Card/Card.jsx'
 import './App.scss'
 
 const App = () => {
+  const users = [
+    { id: 1, userName: 'gab14_', name: 'Gabo', isFollowing: true },
+    { id: 2, userName: 'react', name: 'React', isFollowing: false },
+    { id: 3, userName: 'vite', name: 'Vite', isFollowing: false },
+    { id: 4, userName: 'eslint', name: 'Eslint', isFollowing: true }
+  ]
+
   return (
     <main className='main'>
       <h1 className='main__title'>Twitter Follow Card</h1>
       <aside className='card-groups'>
-        <Card userName='gab14_' name='Gabo' isFollowing />
-        <Card userName='react' name='React' />
-        <Card userName='vite' name='Vite' />
-        <Card userName='eslint' name='Eslint' isFollowing />
+        {users.map(user => (
+          <Card
+            key={user.id}
+            userName={user.userName}
+            name={user.name}
+            isFollowing={user.isFollowing}
+          />
+        ))}
       </aside>
     </main>
   )
